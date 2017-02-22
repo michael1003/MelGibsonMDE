7b550ec2a6bd1fb7da58f6020cc79de8d03f4502
Michael K <bagelbrothersinc@gmail.com>
2017-02-21 20:25:57 -0600

    - practice with a couple notes

---
+++ stuff.js
@@ -0,0 +1,121 @@
+var bigButt = true; 
+
+switch (bigButt) {
+	case false:
+	console.log('Oh papa don\'t like that');
+	break;
+	default:
+	console.log('Oh she a big booty bitch papa like');
+	break;
+	}
+
+var theHole = 'wide'; 
+
+if (theHole === 'wide'); 
+console.log('Nice...');
+
+function takeOrder(topping) {
+console.log('Pizza with ' + topping); 
+}
+
+takeOrder('icecream');
+
+var shitFest = 'There is a shit fest';
+
+if (shitFest === 'There is a shit fest') { 
+console.log('nice');
+} else { 
+console.log('error');
+}
+
+function hamBurger(topping) { 
+console.log('Hamburger with ' + topping)
+}
+
+hamBurger('shit');
+
+hamBurger('turd');
+
+var hamBurger = 'cheese'; 
+console.log(hamBurger);
+
+if (hamBurger === 'cheese') {
+	console.log('chice');
+} else if (hamBurger === 'beef'){
+	console.log('choce');
+}
+
+
+function basicNumber(number) {
+	console.log(number*95 + 'Faggot');
+}
+
+basicNumber(900);
+
+function basicStory(nameOne, nameTwo, placeName) /* different functions seperated by commas */ { 
+console.log(nameOne + ' and ' + nameTwo + ' went to ' + placeName + '.'); 
+}
+
+basicStory('Sam', 'Kyle', 'the beach'); /* each of these corresponds to the basicStory parenthetical and so do the ones below */
+
+basicStory('Matt', 'Andrew', 'a bar and grill');
+
+basicStory('Michael', 'Lucas', 'Jupiter');
+
+var hungerLevel = 10; 
+if (hungerLevel > 5) { 
+console.log('eat some food boy');
+} else if (hungerLevel < 5) { 
+console.log('you\'re good boy');
+
+function takeOrder(topping, crustType) {
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+  orderCount = orderCount + 1;
+}
+
+takeOrder('pepperoni', 'thin crust');
+
+takeOrder('extra cheese', 'thick crust'); 
+
+takeOrder('shrimp', 'deep dish');
+
+var orderCount = 2;
+
+function getSubTotal(itemCount) {
+  return itemCount * 7.5;
+  
+}
+
+console.log(getSubTotal(orderCount));
+
+var orderCount = 2;
+
+function getTotal(furryCount) { 
+return furryCount * 95
+}
+
+console.log(getTotal(orderCount));
+
+var creditNumber = 3
+
+function totalCredit(creditCount) { 
+return creditCount + ' - your credit number'
+}
+
+console.log(totalCredit(creditNumber));
+
+function takeOrder(topping, crustType) {
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+}
+
+takeOrder('diahrea', 'maggot');
+
+function celsiusToFarenheit(celsius) { 
+return celsius * (9/5);
+}
+
+function getFarenheit(celsius) {
+return celsiusToFarenheit(celsius) + 32; 
+}
+
+console.log('The temperature is ' + getFarenheit(12) + ' farenheit');
