e76c526ce6cf49827f23714519687fc9492178e2
Michael K <bagelbrothersinc@gmail.com>
2017-02-24 21:43:50 -0600

    - short bird story

---
+++ bird story.js
@@ -0,0 +1,11 @@
+//bird story
+
+
+var heightCompare = 'shorter';
+
+function birdBoy(color, beak, height) { 
+console.log('Bird boy was a very peculiar boy with ' + color + ' hair and a ' + beak + ' beak. ' + 'He was much ' + heightCompare + ' than his peers at ' + height + ' tall.');
+}
+
+birdBoy('blonde', 'snubbed', 5 + ' foot ' + 5 + ' inches');
+birdBoy('brown', 'long', 5 + ' foot ' + 2 + ' inches');
